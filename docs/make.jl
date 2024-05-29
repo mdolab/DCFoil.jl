aeb8c3f919b6d8a285996ff0b163bd6ccb0d1e6d
gang525 <nggw@umich.edu>
2024-05-29 08:39:17 -0400

    modify make

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,8 @@
 push!(LOAD_PATH, "../src/")
 
 using Documenter
-include("./DCFoil.jl")
-using .DCFoil
+# include("./DCFoil.jl")
+using DCFoil
 
 DocMeta.setdocmeta!(DCFoil, :DocTestSetup, :(using DCFoil); recursive=true)
 
@@ -11,7 +11,7 @@ makedocs(;
     authors="Galen Ng <nggw@umich.edu>",
     sitename="DCFoil.jl",
     format=Documenter.HTML(;
-        canonical="https://mdolab.github.io/DCFoil.jl",
+        canonical="https://gawng.github.io/DCFoil.jl",
         edit_link="main",
         assets=String[],
     ),
@@ -21,6 +21,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/mdolab/DCFoil.jl.git",
+    repo="github.com/gawng/DCFoil.jl.git",
     devbranch="main",
 )
