fix flutter and scaling and run script settings

POSTPROCESSING/read_case.py
@@ -379,7 +379,7 @@ def plot_spanwise():
             actualSpan = aeroNodesXYZ[1, -1]
             print("Actual span:", actualSpan)
             spanXYZ = aeroNodesXYZ[:, -1]
-            dist = np.sqrt(spanXYZ[0]**2+ spanXYZ[1]**2 + spanXYZ[2]**2)  # this is the span from origin
+            dist = np.sqrt(spanXYZ[0] ** 2 + spanXYZ[1] ** 2 + spanXYZ[2] ** 2)  # this is the span from origin
             print("Span distance:", dist)
             sloc, Lprime, gamma_s = compute_elliptical(TotalLift, Uinf, semispan + spanVal, density)
             sloc, Lprime, gamma_s = compute_elliptical(TotalLift, Uinf, actualSpan, density)

dcfoil/SETUP/setup_OMdvgeo.py
@@ -81,8 +81,8 @@ def setup(args, model, comm, files: dict):
             # sweep_ref_pt = C_orig[0, :]
             sweep_ref_pt = C_orig[nSweep + nSkip, :]
 
-            sweep_ref_pt_port = C_orig[nSweep + nSkip-nSkip, :]
-            sweep_ref_pt_stbd = C_orig[nSweep + nSkip+nSkip, :]
+            sweep_ref_pt_port = C_orig[nSweep + nSkip - nSkip, :]
+            sweep_ref_pt_stbd = C_orig[nSweep + nSkip + nSkip, :]
 
             theta = -val[0] * np.pi / 180
             cc = np.cos(theta)